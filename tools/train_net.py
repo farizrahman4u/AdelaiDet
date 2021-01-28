898c2245674a377b716b0c646c898b5bd4c09446
Fariz Rahman <farizrahman4u@gmail.com>
2021-01-28 18:01:01 +0400

    Update train_net.py

--- tools/train_net.py
+++ tools/train_net.py
@@ -14,19 +14,14 @@ Therefore, we recommend you to use detectron2 as an library and take
 this file as an example of how to use the library.
 You may want to write your own script with your datasets and other customizations.
 """
-from detectron2.structures.boxes import Boxes
+
+import wandb
+
 import logging
 import os
 from collections import OrderedDict
 import torch
-from fvcore.nn.precise_bn import get_bn_modules
 from torch.nn.parallel import DistributedDataParallel
-
-from utils.models import add_mnv2_to_registry
-from utils.logs import print_cnfg_as_tags, redirect_outputs, print_experiment, cnvrg_res_print
-from utils.models import print_model_stats
-from callbacks.cnvrg_logs import CnvrgLogger
-import wandb
 
 import detectron2.utils.comm as comm
 from detectron2.data import MetadataCatalog, build_detection_train_loader
@@ -53,48 +48,18 @@ from adet.evaluation import TextEvaluator
 class Trainer(DefaultTrainer):
     """
     This is the same Trainer except that we rewrite the
-    `build_train_loader` method.
+    `build_train_loader`/`resume_or_load` method.
     """
-
-    def __init__(self, cfg):
-        """
-        Args:
-            cfg (CfgNode):
-        Use the custom checkpointer, which loads other backbone models
-        with matching heuristics.
-        """
-        # Assume these objects must be constructed in this order.
-        model = self.build_model(cfg)
-        try:
-            print_model_stats(model, input_size=(550, 550), device=cfg.MODEL.DEVICE)
-        except:
-            print('/could not print model stats')
-        optimizer = self.build_optimizer(cfg, model)
-        data_loader = self.build_train_loader(cfg)
-
-        # For training, wrap with DDP. But don't need this for inference.
-        if comm.get_world_size() > 1:
-            model = DistributedDataParallel(
-                model, device_ids=[comm.get_local_rank()], broadcast_buffers=False
+    def resume_or_load(self, resume=True):
+        if not isinstance(self.checkpointer, AdetCheckpointer):
+            # support loading a few other backbones
+            self.checkpointer = AdetCheckpointer(
+                self.model,
+                self.cfg.OUTPUT_DIR,
+                optimizer=self.optimizer,
+                scheduler=self.scheduler,
             )
-        super(DefaultTrainer, self).__init__(model, data_loader, optimizer)
-
-        self.scheduler = self.build_lr_scheduler(cfg, optimizer)
-        # Assume no other objects need to be checkpointed.
-        # We can later make it checkpoint the stateful hooks
-        self.checkpointer = AdetCheckpointer(
-            # Assume you want to save checkpoints together with logs/statistics
-            model,
-            cfg.OUTPUT_DIR,
-            optimizer=optimizer,
-            scheduler=self.scheduler,
-        )
-        self.start_iter = 0
-        self.max_iter = cfg.SOLVER.MAX_ITER
-        self.cfg = cfg
-
-        default_hooks = self.build_hooks()
-        self.register_hooks(default_hooks)
+        super().resume_or_load(resume=resume)
 
     def train_loop(self, start_iter: int, max_iter: int):
         """
@@ -127,67 +92,6 @@ class Trainer(DefaultTrainer):
             verify_results(self.cfg, self._last_eval_results)
             return self._last_eval_results
 
-    def build_hooks(self):
-
-        """
-        Build a list of default hooks, including timing, evaluation,
-        checkpointing, lr scheduling, precise BN, writing events.
-
-        Returns:
-            list[HookBase]:
-        """
-        cfg = self.cfg.clone()
-        cfg.defrost()
-        cfg.DATALOADER.NUM_WORKERS = 0  # save some memory and time for PreciseBN
-
-        ret = [
-            hooks.IterationTimer(),                                             # 0
-            hooks.LRScheduler(self.optimizer, self.scheduler),                  # 1
-            hooks.PreciseBN(                                                    # 2
-                # Run at the same freq as (but before) evaluation.
-                cfg.TEST.EVAL_PERIOD,
-                self.model,
-                # Build a new data loader to not affect training
-                self.build_train_loader(cfg),
-                cfg.TEST.PRECISE_BN.NUM_ITER,
-            )
-            if cfg.TEST.PRECISE_BN.ENABLED and get_bn_modules(self.model)       # 3
-            else None,
-        ]
-
-        # Do PreciseBN before checkpointer, because it updates the model and need to
-        # be saved by checkpointer.
-        # This is not always the best: if checkpointing has a different frequency,
-        # some checkpoints may have more precise statistics than others.
-        if comm.is_main_process():                                              # 4
-            ret.append(hooks.PeriodicCheckpointer(self.checkpointer, cfg.SOLVER.CHECKPOINT_PERIOD))
-
-        def test_and_save_results():
-            self._last_eval_results = self.test(self.cfg, self.model)
-            return self._last_eval_results
-
-        # Do evaluation after checkpointer, because then if it fails,
-        # we can use the saved checkpoint to debug.
-        eval_hook = hooks.EvalHook(cfg.TEST.EVAL_PERIOD, test_and_save_results)
-        ret.append(eval_hook)
-
-        if comm.is_main_process():
-            # run writers in the end, so that evaluation metrics are written
-            ret.append(hooks.PeriodicWriter(self.build_writers(), period=self.cfg.LOG_INTERVAL))
-            ret.append(CnvrgLogger(log_interval=self.cfg.CNVRG_LOG_INTERVAL))
-
-        return ret
-
-    def test(self, cfg, model, evaluators=None, print_results=True):
-        res = super(Trainer, self).test(cfg, model, evaluators)
-        if print_results:
-            if len(cfg.DATASETS.TEST) > 1:
-                for ds in cfg.DATASETS.TEST:
-                    cnvrg_res_print(res[ds], self.iter, ds_name=ds)
-            else:
-                cnvrg_res_print(res, self.iter)
-        return res
-
     @classmethod
     def build_train_loader(cls, cfg):
         """
@@ -197,11 +101,8 @@ class Trainer(DefaultTrainer):
         It calls :func:`detectron2.data.build_detection_train_loader` with a customized
         DatasetMapper, which adds categorical labels as a semantic mask.
         """
-        from adet.data.builtin import THINGS_ROOT_MAPPING
-        assert len(cfg.DATASETS.TRAIN) == 1
-        things_root = THINGS_ROOT_MAPPING[cfg.DATASETS.TRAIN[0]]
-        mapper = DatasetMapperWithBasis(cfg, things_root, True)
-        return build_detection_train_loader(cfg, mapper)
+        mapper = DatasetMapperWithBasis(cfg, True)
+        return build_detection_train_loader(cfg, mapper=mapper)
 
     @classmethod
     def build_evaluator(cls, cfg, dataset_name, output_folder=None):
@@ -279,94 +180,20 @@ def setup(args):
     return cfg
 
 
-def test_all_checkpoints(trainer, cfg):
-    model = Trainer.build_model(cfg)
-    all_checkpoints = AdetCheckpointer(model, save_dir=cfg.OUTPUT_DIR).get_all_checkpoint_files()
-    print('Test all checkpoints:', all_checkpoints)
-    for checkpoint in sorted(all_checkpoints):
-        if os.path.basename(checkpoint).startswith('model_final'):
-            continue
-        print('Test %s' % checkpoint)
-        AdetCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(checkpoint, resume=False)
-        res = trainer.test(cfg, model, print_results=False)
-        if comm.is_main_process():
-            verify_results(cfg, res)
-        if cfg.TEST.AUG.ENABLED:
-            res.update(Trainer.test_with_TTA(cfg, model))
-        cnvrg_res_print(res, iter=int(os.path.basename(checkpoint)[6:-4]))
-
-def main(args):
-    cfg = setup(args)
-
-# this is my code
-    redirect_outputs(os.path.join(cfg.OUTPUT_DIR, 'log.txt'))
-
-    if comm.is_main_process():
-        print_experiment()
-        print_cnfg_as_tags(cfg)
-        init_wandb(cfg, args)
-
-    add_mnv2_to_registry()
-
-# =============
-
-    trainer = Trainer(cfg)
-
-    if args.eval_all:
-        test_all_checkpoints(trainer, cfg)
-        return
-
-
-
-    if args.eval_only:
-        model = trainer.model
-        AdetCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(
-            cfg.MODEL.WEIGHTS, resume=args.resume
-        )
-        res = trainer.test(cfg, model, print_results=False) # d2 defaults.py
-        if comm.is_main_process():
-            verify_results(cfg, res)
-        if cfg.TEST.AUG.ENABLED:
-            res.update(Trainer.test_with_TTA(cfg, model))
-        cnvrg_res_print(res)
-        return res
-
-    """
-    If you'd like to do anything fancier than the standard training logic,
-    consider writing your own training loop or subclassing the trainer.
-    """
-    trainer.resume_or_load(resume=args.resume)
-    if cfg.TEST.AUG.ENABLED: # this is the last eval
-        trainer.register_hooks(
-            [hooks.EvalHook(0, lambda: trainer.test_with_TTA(cfg, trainer.model))]
-        )
-    return trainer.train()
-
-
-def get_args():
-    parser = default_argument_parser()
-    parser.add_argument("--eval-all", action="store_true", help="perform evaluation to al checkpoints")
-    return parser.parse_args()
+def omega2dict(o):
+    d = {}
+    for k, v in o.items():
+        if type(v) == type(o):
+            d[k] = omega2dict(v)
+        else:
+            d[k] = v
+    return d
 
 
 def init_wandb(cfg, args):
-    from cnvrg import Experiment
-    from utils.logs import flatten_omegaconf
-    try:
-        e = Experiment()
-        name = e['title']
-        url = e['full_href']
-        id = os.path.split(url)[-1]
-    except:
-        name = 'debug'
-        id = 0
-        url = ''
     job_type = 'test' if args.eval_only else 'train'
-    config = flatten_omegaconf(cfg)
-    config['cnvrg_url'] = url
+    config = omega2dict(cfg)
     wandb.init(sync_tensorboard=True,
-               name=name,
-               id=id,
                job_type=job_type,
                config=config,
                dir='output',
@@ -375,25 +202,46 @@ def init_wandb(cfg, args):
                group=cfg.TASK,
                )
 
+def main(args):
+    cfg = setup(args)
+
+    if comm.is_main_process():
+        init_wandb(cfg, args)
+
+    if args.eval_only:
+        model = Trainer.build_model(cfg)
+        AdetCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(
+            cfg.MODEL.WEIGHTS, resume=args.resume
+        )
+        res = Trainer.test(cfg, model) # d2 defaults.py
+        if comm.is_main_process():
+            verify_results(cfg, res)
+        if cfg.TEST.AUG.ENABLED:
+            res.update(Trainer.test_with_TTA(cfg, model))
+        return res
+
+    """
+    If you'd like to do anything fancier than the standard training logic,
+    consider writing your own training loop or subclassing the trainer.
+    """
+    trainer = Trainer(cfg)
+    trainer.resume_or_load(resume=args.resume)
+    if cfg.TEST.AUG.ENABLED:
+        trainer.register_hooks(
+            [hooks.EvalHook(0, lambda: trainer.test_with_TTA(cfg, trainer.model))]
+        )
+    return trainer.train()
+
 
 if __name__ == "__main__":
-    args = get_args()
+    args = default_argument_parser().parse_args()
     print("Command Line Args:", args)
     launch(
         main,
-        num_gpus_per_machine=torch.cuda.device_count(),
+        args.num_gpus,
         num_machines=args.num_machines,
         machine_rank=args.machine_rank,
         dist_url=args.dist_url,
         args=(args,),
     )
-    wandb.finish()
-
-
-# OMP_NUM_THREADS=1 python tools/train_net.py --config-file configs/BlendMask/R_50_1x.yaml --num-gpus 1 --resume OUTPUT_DIR output/blendmask_R_50_1x SOLVER.IMS_PER_BATCH 4
-# OMP_NUM_THREADS=1 python tools/train_net.py --config-file configs/BlendMask/R_50_1x.yaml --num-gpus 4 --resume OUTPUT_DIR output/blendmask_R_50_1x SOLVER.IMS_PER_BATCH 16
-
-
-
-
-# OMP_NUM_THREADS=1 python tools/train_net.py --config-file output/blendmask_R_50_1x/config.yaml --eval-only --num-gpus 1 OUTPUT_DIR output/blendmask_R_50_1x MODEL.WEIGHTS output/blendmask_R_50_1x/model_final.pth
+    # wandb.finish()
