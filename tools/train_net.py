7473656c05403ace52cddb2525564ece5c542cf8
Fariz Rahman <farizrahman4u@gmail.com>
2021-02-07 22:29:51 +0400

    Update train_net.py

--- tools/train_net.py
+++ tools/train_net.py
@@ -231,10 +231,10 @@ def main(args):
     """
     trainer = Trainer(cfg)
     trainer.resume_or_load(resume=args.resume)
-    if cfg.TEST.AUG.ENABLED:
-        trainer.register_hooks(
-            [hooks.EvalHook(0, lambda: trainer.test_with_TTA(cfg, trainer.model))]
-        )
+#     if cfg.TEST.AUG.ENABLED:
+#         trainer.register_hooks(
+#             [hooks.EvalHook(0, lambda: trainer.test_with_TTA(cfg, trainer.model))]
+#         )
     return trainer.train()
 
 
